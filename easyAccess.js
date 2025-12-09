ef69866cc175b37f161292b7187d43fe4cf4b3b5
ChkChkChkBoom <247869555+ChkChkChkBoom@users.noreply.github.com>
2025-12-09 08:27:02 -0800

    Significant improvements to easyAccess

--- easyAccess.js
+++ easyAccess.js
@@ -1,36 +1,40 @@
 /** @param {NS} ns */
 export async function main(ns) {
-  const mode = (ns.args[0]||"list")
+  const mode = (ns.args[0]||"help")
+  //Original repo (don't change)
+  const ORIGINALREPO="ZianConradie/Bitburner-scripts"
+  //You can change this though
+  const repo = "ZianConradie/Bitburner-scripts"
   switch (mode) {
-    case "list":
-      ns.tprint(await getFiles(ns))
+    case "list": {
+      ns.tprint(await getFiles(ns,repo))
       break
-
-    case "show":
+    }
+    case "show": {
       let fileName0 = ns.args[1]
-      let u0 = "https://raw.githubusercontent.com/ZianConradie/Bitburner-scripts/main/" + fileName0
+      let u0 = "https://raw.githubusercontent.com/"+repo+"/main/" + fileName0
       let re0 = await fetch(u0)
       if (!re0.ok) throw new Error("Failed to fetch file")
       let data0 = await re0.text()
       ns.tprint(data0)
       break
-
-    case "download":
+      }
+    case "download": {
       let fileName1 = ns.args[1]
-      let u1 = "https://raw.githubusercontent.com/ZianConradie/Bitburner-scripts/main/" + fileName1
+      let u1 = "https://raw.githubusercontent.com/"+repo+"/main/" + fileName1
       let re1 = await fetch(u1)
       if (!re1.ok) throw new Error("Failed to fetch file")
       let data1 = await re1.text()
       ns.write(fileName1, data1, "w")
       ns.tprint("File Created")
       break
-
+    }
     case "downloadall": {
       const files = await getFiles()
       ns.tprint(`Found ${files.length} files. Starting download...`)
 
       for (const file of files) {
-        const url = "https://raw.githubusercontent.com/ZianConradie/Bitburner-scripts/main/" + file
+        const url = "https://raw.githubusercontent.com/"+repo+"/main/" + file
         const res = await fetch(url)
 
         if (!res.ok) {
@@ -49,19 +53,40 @@ export async function main(ns) {
       ns.tprint("🎉 Finished downloading all files!")
       break
     }
-
-    case "credits":
-      ns.tprint("\nMade by ChkChkChkBoom (@chkchkchkboom on Discord)")
+    //This works, I think
+    case "repository":
+    case "repo": {
+      ns.tprint("Current repository: "+repo)
       break
-
-    case "help":
-      ns.tprint("\nCommands:\nhelp: displays help\ncredits: shows credits\nlist: display file tree\nshow: display a file\ndownload: downloads a file to Bitburner\ndownloadall: downloads ALL files except LICENSE & README.md")
+    }
+    case "credits": {
+      //Replace with your name if you use this elsewhere
+      const adaptorName="ChkChkChkBoom"
+      //Replace with contact info
+      const adaptorContact="@chkchkchkboom on discord"
+      //DON'T CHANGE THIS
+      const origName="Made by ChkChkChkBoom (@chkchkchkboom on discord)"
+      let str=""
+      //Don't change
+      if (adaptorName=="ChkChkChkBoom"){
+        str+="\n"+origName
+      }
+      else{
+        //Don't change
+        str+="\nAdapted by "+adaptorName+" ("+adaptorContact+"). "+origName+". Original repository was ZianConradie/Bitburner-scripts"
+      }
+      ns.tprint(str)
       break
+    }
+    case "help": {
+      ns.tprint("\nCommands:\nhelp: displays help\ncredits: shows credits\nlist: display file tree\nshow: display a file\ndownload: downloads a file to Bitburner\ndownloadall: downloads ALL files except LICENSE & README.md\nrepo (or repository): displays repository used")
+      break
+    }
   }
 }
 
-async function getFiles() {
-    const url = "https://api.github.com/repos/ZianConradie/Bitburner-scripts/git/trees/main?recursive=1"
+async function getFiles(ns,repo) {
+    const url = "https://api.github.com/repos/"+repo"+/git/trees/main?recursive=1"
     const res = await fetch(url)
 
     if (!res.ok) {
